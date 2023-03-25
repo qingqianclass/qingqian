0f2c3e68281f7b84433119c879480ae3108f9208
qingqian <2782207003@qq.com>
2023-03-25 14:05:21 +0800

    第一次

---
+++ src/com/runajian2202/dao/LockDao.java
@@ -0,0 +1,29 @@
+package com.runajian2202.dao;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+
+public class LockDao {
+    //查询锁定状态
+    public static ResultSet checkLock(Connection conn, String uername) throws SQLException {
+        String sql="select * from user_lock where username=?";
+        PreparedStatement ps=conn.prepareStatement(sql);
+        ps.setString(1,uername);
+        return ps.executeQuery();
+    }
+    //锁定
+    public static int lockUrename(Connection conn, String name, LocalDateTime t) throws SQLException {
+        String sql="INSERT INTO user_lock (username, lock_time) VALUES (?, ?)";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, name);
+        ps.setTimestamp(2, Timestamp.valueOf(t));
+        return ps.executeUpdate();
+    }
+    //解锁
+    public static int unlockUrename(Connection conn,String name) throws SQLException {
+        String sql="DELETE FROM user_lock WHERE username = ?";
+        PreparedStatement ps=conn.prepareStatement(sql);
+        ps.setString(1, name);
+        return ps.executeUpdate();
+    }
+}
